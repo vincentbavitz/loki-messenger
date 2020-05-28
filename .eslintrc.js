c253f5b53a8576452326b8e68fb835201d693423
Vincent <vincent@loki.network>
2020-05-28 13:58:14 +1000

    Rewrote eslint and tslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,72 +1,159 @@
-// For reference: https://github.com/airbnb/javascript
+/* Reconfigured to allow ESLint to handle TypeScript^3.9 */
 
 module.exports = {
-  settings: {
-    'import/core-modules': ['electron'],
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
   },
-
-  extends: ['airbnb-base', 'prettier'],
-
-  plugins: ['mocha', 'more'],
-
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', '@typescript-eslint', 'react-hooks'],
   rules: {
-    'comma-dangle': [
-      'error',
+    'prettier/prettier': [
+      0,
       {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'never',
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'es5',
+        printWidth: 100,
+        tabWidth: 2,
+        arrowParens: 'avoid',
+        jsxSingleQuote: false,
+        jsxBracketSameLine: true,
       },
     ],
 
-    // Enforce curlies always
-    curly: ['error', 'all'],
-    'brace-style': ['error', '1tbs'],
-
-    // prevents us from accidentally checking in exclusive tests (`.only`):
-    'mocha/no-exclusive-tests': 'error',
-
-    // encourage consistent use of `async` / `await` instead of `then`
-    'more/no-then': 'error',
-
-    // it helps readability to put public API at top,
-    'no-use-before-define': 'off',
-
-    // useful for unused or internal fields
-    'no-underscore-dangle': 'off',
-
-    // though we have a logger, we still remap console to log to disk
-    'no-console': 'error',
-
-    // consistently place operators at end of line except ternaries
-    'operator-linebreak': 'error',
-
-    // Use LF to stay consistent
-    'linebreak-style': ['error', 'unix'],
-
-    quotes: [
-      'error',
-      'single',
-      { avoidEscape: true, allowTemplateLiterals: true },
-    ],
-
-    // Prettier overrides:
-    'arrow-parens': 'off',
-    'function-paren-newline': 'off',
-    'max-len': [
-      'error',
+    semi: 0,
+    eqeqeq: [1, 'always'],
+    quotes: [1, 'single'],
+    'jsx-quotes': [2, 'prefer-double'],
+    'no-undef': 0,
+    'no-console': 1,
+    'no-unused-vars': 0,
+    'no-mixed-operators': [
+      1,
       {
-        // Prettier generally limits line length to 80 but sometimes goes over.
-        // The `max-len` plugin doesn’t let us omit `code` so we set it to a
-        // high value as a buffer to let Prettier control the line length:
-        code: 999,
-        // We still want to limit comments as before:
-        comments: 150,
-        ignoreUrls: true,
-        ignoreRegExpLiterals: true,
+        allowSamePrecedence: true,
       },
     ],
+    'eol-last': [2, 'always'],
+    'no-confusing-arrow': 0,
+    'arrow-parens': [1, 'as-needed'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'arrow-body-style': [2, 'as-needed'],
+    'no-extra-parens': [
+      'warn',
+      'all',
+      {
+        conditionalAssign: false,
+        nestedBinaryExpressions: false,
+        ignoreJSX: 'none',
+        enforceForArrowConditionals: false,
+      },
+    ],
+    'no-console': 1,
+    'no-underscore-dangle': 0,
+    'no-use-before-define': 0,
+    'no-continue': 0,
+    'no-extra-parens': 0,
+    'no-param-reassign': 0,
+    'prefer-template': 0,
+    'prefer-promise-reject-errors': 0,
+    'no-script-url': 0,
+    'prefer-promise-reject-errors': 0,
+    'no-unused-expressions': 0,
+
+    'import/prefer-default-export': 0,
+    'import/no-useless-path-segments': 1,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/no-duplicates': 0,
+    'import/order': 0,
+    'import/newline-after-import': 1,
+    'import/no-named-as-default-member': 0,
+    'import/namespace': 0,
+    'import/named': 0,
+
+    'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/interactive-supports-focus': 0,
+    'react/jsx-closing-bracket-location': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/label-has-for': 0,
+    'jsx-a11y/iframe-has-title': 0,
+    'jsx-a11y/control-has-associated-label': 0,
+
+    'react/jsx-wrap-multilines': [
+      2,
+      {
+        declaration: 'parens-new-line',
+        assignment: 'parens-new-line',
+        return: 'parens-new-line',
+        arrow: 'ignore',
+        condition: 'ignore',
+        logical: 'ignore',
+        prop: 'ignore',
+      },
+    ],
+    'react/jsx-filename-extension': 0,
+    'react/jsx-indent': 0,
+    'react/jsx-boolean-value': 0,
+    'react/jsx-closing-tag-location': 0,
+    'react/jsx-closing-bracket-location': [
+      2,
+      { selfClosing: 'props-aligned', nonEmpty: 'after-props' },
+    ],
+    'react/button-has-type': 0,
+    'react/prop-types': 0,
+    'react/button-has-type': 0,
+    'react/jsx-tag-spacing': [2, { beforeSelfClosing: 'always' }],
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-curly-spacing': 0,
+    'react/no-access-state-in-setstate': 0,
+    'react/destructuring-assignment': 0,
+    'react/jsx-no-bind': 0,
+    'react/require-default-props': 0,
+    'react/display-name': 0,
+    'react/jsx-first-prop-new-line': 0,
+    'react/prefer-stateless-function': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/static-property-placement': 0,
+    'react/state-in-constructor': 0,
+
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/member-delimiter-style': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/no-angle-bracket-type-assertion': 0,
   },
 };
